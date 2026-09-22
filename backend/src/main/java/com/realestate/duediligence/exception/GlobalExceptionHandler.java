71827c4f77163a4f7f3491955eb76b965615b644
bhaskarvadde <bhaskarvadde.dev@gmail.com>
2026-09-22 21:37:55 +0530

    updated application.prop and exception

--- backend/src/main/java/com/realestate/duediligence/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/realestate/duediligence/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.realestate.duediligence.exception;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus; 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
